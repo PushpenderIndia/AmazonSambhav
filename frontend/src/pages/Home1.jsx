89d6f9fc4e5b44d184773d4fde49418d6d3ae8a5
Pushpender Singh <singhpushpender250@gmail.com>
2024-11-19 19:31:38 +0530

    lol

---
+++ frontend/src/pages/Home1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginComponent from '../components/LoginComponent'
+import Header from '../components/Header'
+function Home() {
+  return (
+    <div>
+        <LoginComponent></LoginComponent>
+    </div>
+  )
+}
+
+export default Home
