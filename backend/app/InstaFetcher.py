2638edb3b031b439a6d1007fa8c7629076085413
Pushpender Singh <singhpushpender250@gmail.com>
2024-11-24 20:57:44 +0530

    lol

--- backend/app/InstaFetcher.py
+++ backend/app/InstaFetcher.py
@@ -1,4 +1,24 @@
-
+import instaloader
 
 class InstaFetcher:
-    
+    loader = instaloader.Instaloader()
+
+    def fetch(self, username):
+        try:
+            profile = instaloader.Profile.from_username(self.loader.context, username)
+            posts = profile.get_posts()
+            for i, post in enumerate(posts):
+                if i >= 5:
+                    break
+                print(post.url)
+            # for i, post in enumerate(posts):
+            #     if i >= 5:
+            #         break
+            #     print(f"Downloading post {i+1} - {post.url}")
+            #     self.loader.download_post(post, target=f"{username}_post_{i+1}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    fetcher = InstaFetcher()
+    fetcher.fetch("bata.india")
