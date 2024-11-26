43be4c0ff6938a327681ae0352539b6c6409a405
Pushpender Singh <singhpushpender250@gmail.com>
2024-11-26 14:30:17 +0530

    lol

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -37,7 +37,7 @@ const Dashboard: React.FC = () => {
         }
     };
 
-    //   Fetch Dashboard stats
+    // Fetch Dashboard stats
     const [dashboardStats, setDashboardStats] = useState({
         total_listings: "Loading...",
         approved_listings: "Loading...",
