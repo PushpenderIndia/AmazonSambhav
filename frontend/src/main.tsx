0ee77754da8398f69e567c87206202749695d4b3
Pushpender Singh <singhpushpender250@gmail.com>
2024-11-26 16:19:26 +0530

    lol

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import routing tools
-// import App from "./App.tsx";
-import HomePage from "./pages/HomePage.tsx"; // Import the HomePage component
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import HomePage from "./pages/HomePage.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import LinkSocialMedia from "./pages/LinkSocialMedia.tsx";
+import PrivateRoute from "./PrivateRoute"; // Import the PrivateRoute component
+import "./index.css";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -21,8 +21,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} /> 
-          <Route path="/dashboard" element={<Dashboard/>} />  
-          <Route path="/linksocialmedia" element={<LinkSocialMedia/>} />
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />  
+          <Route path="/linksocialmedia" element={<PrivateRoute element={<LinkSocialMedia />} />} />
         </Routes>
       </Router>
     </ClerkProvider>
